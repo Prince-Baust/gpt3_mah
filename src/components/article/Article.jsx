fbe6ab6e798ca64640c2314597dfb5b9cbf0f142
Maruf Al Hossain <pmaruf42@gmail.com>
2022-05-23 13:37:44 +0600

    article style complete

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -4,7 +4,7 @@ import './article.css';
 const Article = ({imgUrl, date, title}) => {
   return (
     <div className="gpt3__blog-container_article">
-      <div className="gpt3__blo-container_article-image">
+      <div className="gpt3__blog-container_article-image">
         <img src={imgUrl} alt="Blog"/>
       </div>
       <div className="gpt3__blog-container_article-content">
